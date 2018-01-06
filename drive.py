854b3b2670adaf8eb166469dd42b9713b1417157
Matthew Cluver <matthew.cluver@gmail.com>
2018-01-06 12:57:10 -0500

    Create drive.py

---
+++ drive.py
@@ -0,0 +1,42 @@
+from time import sleep
+import RPi.GPIO as GPIO
+
+DIR = 20       # Direction GPIO Pin
+STEP = 21      # Step GPIO Pin
+CW = 1         # Clockwise Rotation
+CCW = 0        # Counterclockwise Rotation
+SPR = 200       # Steps per Revolution (360 / 1.8)
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(DIR, GPIO.OUT)
+GPIO.setup(STEP, GPIO.OUT)
+GPIO.output(DIR, CW)
+
+MODE = (14, 15, 18) # Microstep Resolution GPIO Pins
+GPIO.setup(MODE, GPIO.OUT)
+RESOLUTION = {'Full': (0, 0, 0),
+              'Half': (1, 0, 0),
+              '1/4': (0, 1, 0),
+              '1/8': (1, 1, 0),
+              '1/16': (0, 0, 1),
+              '1/32': (1, 0, 1)}
+
+GPIO.output(MODE, RESOLUTION['1/32'])
+
+step_count = SPR * 4
+delay = .005 / 32
+
+for x in range(step_count):
+    GPIO.output(STEP, GPIO.HIGH)
+    sleep(delay)
+    GPIO.output(STEP, GPIO.LOW)
+    sleep(delay)
+
+sleep(.5)
+GPIO.output(DIR, CCW)
+
+for x in range(step_count):
+    GPIO.output(STEP, GPIO.HIGH)
+    sleep(delay)
+    GPIO.output(STEP, GPIO.LOW)
+    sleep(delay)
